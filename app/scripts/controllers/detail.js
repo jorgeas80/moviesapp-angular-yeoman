b7610d8b24fcfec28091f70d0eb29f6953f4ab7d
Jorge Arévalo <jorgeas80@gmail.com>
2016-06-07 17:26:46 +0200

    Explanation comment added

--- app/scripts/controllers/detail.js
+++ app/scripts/controllers/detail.js
@@ -9,7 +9,17 @@
  */
 angular.module('pelitweetsAngularYeomanApp')
   .controller('DetailCtrl', function ($routeParams, getMoviesFactory) {
-    var vm = this;
 
-    vm.movie = getMoviesFactory.getMovieDetail($routeParams.id);
+      /**
+       * this.movie will hold the result of a $resource(...).get() call. So, there will be 2 phases:
+       *
+       *  1. At first, the object will contain a promise and a field named $resolved with value 'false', to indicate
+       *  the promise is still not solved
+       *  2. When the data arrives, the field $resolved becomes true, and the object will be transformed in a JSON
+       *  object containing just the data. So, we can assume this will be a movie object
+       *
+       * Check the template code, to see how we use movie.$resolved to show/hide a div, and how we just use movie
+       * as a plain json object when needed
+       **/
+    this.movie = getMoviesFactory.getMovieDetail($routeParams.id);
   });
